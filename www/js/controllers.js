ac72d5aa8e521ce2300454addef60e3623952a6c
Qi Qi <qiqiatosu@gmail.com>
2015-07-25 22:29:59 -0700

    Update front page a little

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,9 @@
 angular.module('yuefan.controllers', ['yuefan.services'])
 
-.controller('FanjuCtrl', ['$scope', 'FanjuSrv', function($scope, FanjuSrv){
+.controller('FanjuCtrl', [
+  '$scope',
+  'FanjuSrv',
+  function($scope, FanjuSrv){
   FanjuSrv.getAll().then(
     function(results){
       $scope.fanjus = results;
