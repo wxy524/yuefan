91a6aeab3812e51e7becd81ab9469e64dc086d8e
wxy524 <wangxueyi0524@gmail.com>
2015-08-18 15:52:29 -0700

    intermediate impl

--- www/view_login/login.js
+++ www/view_login/login.js
@@ -35,13 +35,26 @@
                 }
             );
         };
+
+        $scope.facebook = function(){
+            LoginSrv.facebook().then(
+                function(){
+                  $state.go('fanjus');
+                },
+                function(error){
+                  $sope.incorret = true;
+                  $state.go('login');
+                }
+            );
+        };  
     }
 
     function LoginSrv ($q) {
         var self = this;
 
         self.signup = signup;
-        self.login = login;
+        self.login = login; 
+        self.facebook = facebook;
 
         function signup (user_) {
             var user = new Parse.User();
@@ -76,5 +89,50 @@
             });
             return defer.promise;
         };
-    }
+
+        function facebook () {
+          
+          (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if(d.getElementById(id)) {
+                  return;
+              }
+              js = d.createElement(s);
+              js.id = id;
+              js.src = "//connect.facebook.net/en_US/sdk.js";
+              fjs.parentNode.insertBefore(js, fjs);
+          }(document, 'script', 'facebook-jssdk'));                                        
+
+          window.fbAsyncInit = function() {
+           Parse.FacebookUtils.init({ // this line replaces FB.init({
+                    appId      : '421761141364404', // Facebook App ID
+                    status     : true,  // check Facebook Login status
+                    cookie     : true,  // enable cookies to allow Parse to access the session
+                    xfbml      : true,  // initialize Facebook social plugins on the page
+                    version    : 'v2.3' // point to the latest Facebook Graph API version
+                });
+
+                var defer = $q.defer();
+                Parse.FacebookUtils.logIn(null, {
+                    success: function(user) {
+                        defer.resolve(user);    
+                    },
+                    error: function(user, error) {
+                        defer.reject(error);
+                    }
+                });
+                return defer.promise;
+            } 
+            /*(function(d, s, id){
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) {
+                    return;
+                }
+                js = d.createElement(s); 
+                js.id = id;
+                js.src = "//connect.facebook.net/en_US/sdk.js";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));*/
+        } 
+    }     
 })();
