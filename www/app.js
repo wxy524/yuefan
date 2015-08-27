d0dae366ac360503466888d677bac736433423cf
wxy524 <wangxueyi0524@gmail.com>
2015-08-27 13:24:43 -0700

    save code

--- www/app.js
+++ www/app.js
@@ -34,24 +34,24 @@ angular
                 $state.go('login');
             }
 
-/*            $window.fbAsyncInit = function() {*/
-                //Parse.FacebookUtils.init({ // this line replaces FB.init({
-                    //appId      : '1574365689555899', // Facebook App ID
-                    //status     : true,  // check Facebook Login status
-                    //cookie     : false,  // enable cookies to allow Parse to access the session
-                    //xfbml      : true,  // initialize Facebook social plugins on the page
-                    //version    : 'v2.4' // point to the latest Facebook Graph API version
-                //});
+            /*$window.fbAsyncInit = function() {
+                 Parse.FacebookUtils.init({   this line replaces FB.init({
+                    appId      : '1574365689555899', // Facebook App ID
+                    status     : true,  // check Facebook Login status
+                    cookie     : false,  // enable cookies to allow Parse to access the session
+                    xfbml      : true,  // initialize Facebook social plugins on the page
+                    version    : 'v2.4' // point to the latest Facebook Graph API version
+                });
 
-            //};
+            };
 
-            //(function(d, s, id){
-                //var js, fjs = d.getElementsByTagName(s)[0];
-                //if (d.getElementById(id)) {return;}
-                //js = d.createElement(s); js.id = id;
-                //js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4&appId=1574365689555899";
-                //fjs.parentNode.insertBefore(js, fjs);
-            /*}(document, 'script', 'facebook-jssdk'));*/
+            (function(d, s, id){
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) {return;}
+                js = d.createElement(s); js.id = id;
+                js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4&appId=1574365689555899";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));*/
         });
     }
 
